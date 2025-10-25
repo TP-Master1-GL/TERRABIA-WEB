44ccdf068a4f80ab0a084c619402408a100ccbdf
maamoc ronel <ronelmaamoc52@gmail.com>
2025-10-25 12:23:16 +0100

    ajout des tests unitaire

--- terra-auth-service/auth_app/urls.py
+++ terra-auth-service/auth_app/urls.py
@@ -1,10 +1,11 @@
 # auth_app/urls.py
 from django.urls import path
-from .views import LoginView, RefreshView, ValidateView, LogoutView
+from . import views
 
 urlpatterns = [
-    path('auth/login', LoginView.as_view()),
-    path('auth/refresh', RefreshView.as_view()),
-    path('auth/validate', ValidateView.as_view()),
-    path('auth/logout', LogoutView.as_view()),
+    path('login/', views.LoginView.as_view(), name='login'),         # ← name='login'
+    path('refresh/', views.RefreshView.as_view(), name='refresh'),   # ← name='refresh'
+    path('validate/', views.ValidateView.as_view(), name='validate'), # ← name='validate'
+    path('logout/', views.LogoutView.as_view(), name='logout'),      # ← name='logout'
+    # path('register/', views.RegisterView.as_view(), name='register'),
 ]
